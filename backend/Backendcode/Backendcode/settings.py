7edb31103c54a079bc3417f89209755f3178290b
Abeer Kapoor <abrkapoor@gmail.com>
2026-06-15 15:47:40 +0530

    do to sqlite

--- backend/Backendcode/Backendcode/settings.py
+++ backend/Backendcode/Backendcode/settings.py
@@ -89,12 +89,8 @@ WSGI_APPLICATION = 'Backendcode.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Restroscan_db',
-        'USER': 'postgres',
-        'PASSWORD': 'rootroot',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
